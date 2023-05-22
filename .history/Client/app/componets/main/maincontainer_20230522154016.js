22656e8a6c7cb27af7f675aa13f84c69f229b006
Parth Sapar <parthsapar2@gmail.com>
2023-05-22 22:58:53 +0530

    Actual Project

---
+++ .history/Client/app/componets/main/maincontainer_20230522154016.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import Main from "./main.jsx";
+
+const mapStateToProps = ({
+
+}) => ({
+
+});
+
+const mapDispatchToProps = () => dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
